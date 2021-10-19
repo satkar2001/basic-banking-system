e9dd033d9686ded07fd9716a6beeca7265174596
satkar2001 <satkaracharya883@gmail.com>
2021-10-19 16:22:56 +0530

    history change

---
+++ history.php
@@ -0,0 +1,45 @@
+<?php include('constants.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basic-banking-system</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <link rel="stylesheet" href="style1.css">
+
+</head>
+<body>
+
+
+
+<header>
+
+
+
+    <div class="header">
+
+        <div id="menu-bar" class="fas fa-bars"></div>
+        <h1>SATKAR BANK</h1>
+        <nav class="navbar">
+            <a href="index.php">home</a>
+            <a href="transactions.php">transfer</a>
+            <a href="history.php">view history</a>
+        </nav>
+
+
+    </div>
+
+</header>
+<div class="main-content">
+<div class='wrapper'>
+
+
+</div>
+</div>
+
+</body>
+</html>
